9fae6569914cf1d60b368e6b67f53010fc52afeb
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2020-04-02 09:14:54 +0200

    Fix babel transpile settings
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- webpack.common.js
+++ webpack.common.js
@@ -38,7 +38,7 @@ module.exports = {
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
-				exclude: /node_modules/,
+				exclude: /node_modules(?!(\/|\\)(camelcase|fast-xml-parser|hot-patcher|webdav)(\/|\\))/,
 			},
 			{
 				test: /\.(png|jpg|gif|svg)$/,
