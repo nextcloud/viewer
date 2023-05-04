1c7394c58c943983d171c05c82b2b57fcace0921
Grigorii K. Shartsev <me@shgk.me>
2023-05-04 08:45:37 +0000

    fix: use left position of sidebar to set viewer width
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -55,7 +55,7 @@
 		:has-previous="hasPrevious"
 		:inline-actions="canEdit ? 1 : 0"
 		:spread-navigation="true"
-		:style="{ width: isSidebarShown ? `calc(100% - ${sidebarWidth}px)` : null }"
+		:style="{ width: isSidebarShown ? `${sidebarPosition}px` : null }"
 		:title="currentFile.basename"
 		:view="currentFile.modal"
 		class="viewer"
@@ -219,7 +219,7 @@ export default {
 			cancelRequestFolder: () => {},
 
 			// Flags
-			sidebarWidth: 0,
+			sidebarPosition: 0,
 			isSidebarShown: false,
 			canSwipe: true,
 			isStandalone: !(OCA && OCA.Files && 'fileActions' in OCA.Files),
@@ -921,9 +921,10 @@ export default {
 			this.isSidebarShown = true
 			const sidebar = document.querySelector('aside.app-sidebar')
 			if (sidebar) {
-				this.sidebarWidth = sidebar.offsetWidth
+				this.sidebarPosition = sidebar.getBoundingClientRect().left
 				this.trapElements = [sidebar]
 			}
+			this.updateSidebarPosition()
 		},
 
 		handleAppSidebarClose() {
@@ -931,11 +932,10 @@ export default {
 			this.trapElements = []
 		},
 
-		onResize(event) {
-			// update sidebar width
+		onResize() {
 			const sidebar = document.querySelector('aside.app-sidebar')
 			if (sidebar) {
-				this.sidebarWidth = sidebar.offsetWidth
+				this.sidebarPosition = sidebar.getBoundingClientRect().left
 			}
 		},
 
