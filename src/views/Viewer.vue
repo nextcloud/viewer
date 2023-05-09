4b967d43148e63174a8c18197ec767d6d0902b42
Grigorii K. Shartsev <me@shgk.me>
2023-05-09 13:15:48 +0200

    fix: remove undefined method call
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -924,7 +924,6 @@ export default {
 				this.sidebarPosition = sidebar.getBoundingClientRect().left
 				this.trapElements = [sidebar]
 			}
-			this.updateSidebarPosition()
 		},
 
 		handleAppSidebarClose() {
