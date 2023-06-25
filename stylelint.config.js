b7b746ccee2b0e468f01f23ef890d7e5eb193e75
nextcloud-command <nextcloud-command@users.noreply.github.com>
2023-06-25 03:53:54 +0000

    chore(deps): fix npm audit
    Signed-off-by: GitHub <noreply@github.com>
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,11 @@ const stylelintConfig = require('@nextcloud/stylelint-config')
 
 // Disable nested scss import
 stylelintConfig.rules['no-invalid-position-at-import-rule'] = null
+stylelintConfig.rules['selector-pseudo-element-no-unknown'] = [
+	true,
+	{
+	  ignorePseudoElements: ['v-deep'],
+	},
+]
 
 module.exports = stylelintConfig
