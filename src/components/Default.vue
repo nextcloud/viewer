44c0e03d97e488e8fcd5357a94c3dd6a1c7884cf
Misha M.-Kupriyanov <kupriyanov@strato.de>
2024-08-27 17:16:53 +0200

    feat: add default component stub
    in order later to use it as default viewer
    
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

---
+++ src/components/Default.vue
@@ -0,0 +1,22 @@
+<!--
+  - SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-FileCopyrightText: 2024 STRATO AG
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+<template>
+	<div class="default_container">
+		coming soon
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Default',
+}
+</script>
+
+<style scoped lang="scss">
+.default_container {
+	color: #fff;
+}
+</style>
