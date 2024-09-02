7dd70b17fcbf82743a843af0192b516ab9b6c9aa
Franziska Bath <franziska.bath@strato.de>
2024-09-02 13:37:47 +0200

    feat: properly implement default component
    Signed-off-by: Franziska Bath <franziska.bath@strato.de>

--- src/components/Default.vue
+++ src/components/Default.vue
@@ -5,18 +5,62 @@
 -->
 <template>
 	<div class="default_container">
-		coming soon
+		<img :src="mimeIcon" alt="mimetype-icon">
+		<span class="title">
+			{{ basename }}
+		</span>
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'Default',
+
+	props: {
+		basename: {
+			type: String,
+			default: '',
+		},
+		mime: {
+			type: String,
+			default: '',
+		},
+	},
+
+	computed: {
+		mimeIcon() {
+			return OC.MimeType.getIconUrl(this.mime);
+		}
+	},
+
+	mounted() {
+		this.doneLoading();
+	},
 }
 </script>
 
 <style scoped lang="scss">
 .default_container {
-	color: #fff;
+	display: flex;
+	align-items: center;
+	height: 100%;
+	justify-content: center;
+	flex-direction: column;
+}
+
+img {
+	align-self: center;
+	justify-self: center;
+	margin-bottom: 16px;
+	width: 100%;
+	height: 10em;
+}
+
+.title {
+	font-weight: bold;
+	font-size: 1.4em;
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
 }
 </style>
