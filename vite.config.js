540350a5de9c8d814f051fa21d37ffb8a90c0a89
Ferdinand Thiessen <opensource@fthiessen.de>
2024-09-09 13:40:02 +0200

    chore: Adjust vite config to correctly clear old CSS files
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import { join } from 'node:path'
 const isProduction = process.env.NODE_ENV === 'production'
 const plyrIcons = readFileSync(join(__dirname, 'node_modules', 'plyr', 'dist', 'plyr.svg'), { encoding: 'utf8' })
 
+const isProduction = process.env.NODE_ENV !== 'development'
+
 export default createAppConfig({
 	main: 'src/main.js',
 }, {
@@ -12,4 +14,10 @@ export default createAppConfig({
 		PLYR_ICONS: JSON.stringify(plyrIcons),
 	},
 	minify: isProduction,
+	// ensure that every JS entry point has a matching CSS file
+	createEmptyCSSEntryPoints: true,
+	// Make sure we also clear the CSS directory
+	emptyOutputDirectory: {
+		additionalDirectories: ['css'],
+	},
 })
