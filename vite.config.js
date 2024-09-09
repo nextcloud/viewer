ad44f523943bef4ecb678a4f7452aeb311cb7681
Ferdinand Thiessen <opensource@fthiessen.de>
2024-09-09 12:55:40 +0200

    fix: Make loading the viewer an init script
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { createAppConfig } from '@nextcloud/vite-config'
 import { readFileSync } from 'node:fs'
 import { join } from 'node:path'
 
-const isProduction = process.env.NODE_ENV === 'production'
-const plyrIcons = readFileSync(join(__dirname, 'node_modules', 'plyr', 'dist', 'plyr.svg'), { encoding: 'utf8' })
-
 const isProduction = process.env.NODE_ENV !== 'development'
+const plyrIcons = readFileSync(join(__dirname, 'node_modules', 'plyr', 'dist', 'plyr.svg'), { encoding: 'utf8' })
 
 export default createAppConfig({
 	main: 'src/main.js',
+	init: 'src/init.ts',
 }, {
 	replace: {
 		PLYR_ICONS: JSON.stringify(plyrIcons),
