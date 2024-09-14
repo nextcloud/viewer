3b28060ccaf8fba7c076cc8214299fa7b40a7ed0
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2024-09-14 14:45:58 +0200

    fix: babel config
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 const babelConfig = require('@nextcloud/babel-config')
 
 babelConfig.plugins.push('@babel/plugin-proposal-class-properties')
+babelConfig.plugins.push('@babel/plugin-transform-private-methods')
 
 module.exports = babelConfig
