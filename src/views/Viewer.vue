0538c2637e51e153dc6951169e0e473c2aa68c22
Louis Chmn <louis@chmn.me>
2025-10-14 16:14:06 +0200

    fix: Cast fileid to int before building the Node object
    - Follow-up of https://github.com/nextcloud/viewer/pull/2988
    
    Signed-off-by: Louis Chmn <louis@chmn.me>

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -1124,7 +1124,7 @@ export default defineComponent({
 
 		async onDelete() {
 			try {
-				const fileid = this.currentFile.fileid
+				const fileid = Number.parseInt(this.currentFile.fileid)
 				const url = this.currentFile.source ?? this.currentFile.davPath
 
 				// Fake node to emit the event until Viewer is migrated to the new Node API.
